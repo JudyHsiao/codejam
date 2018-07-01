7f22c1a5a087e9f2966d174c6ee7eea11cca6f4c
JudyHsiao <cornsoup1984@gmail.com>
2018-07-02 07:57:02 +0800

    add case

--- uva/727.py
+++ uva/727.py
@@ -9,23 +9,13 @@ if len(sys.argv) > 1:
     inp = ''.join(open(filename, "r").readlines())
     sys.stdin = io.StringIO(inp)
 
-def valid(A):
-    S = []
-    for a in A:
-        #print(a)
-        if a =='(' or a =='[':
-            S.append(a)
-        elif a ==']':
-            if len(S) == 0 or  '[' != S.pop():
-                return False
-        elif a ==')':
-          if len(S) == 0 or  '(' != S.pop():
-                return False
-
-    if len(S) ==0:
-        return True
-
-    return False
+def prec(c):
+    if c == '+' or c == '-':
+        return 1
+    elif c == '*' or c == '/':
+        return 2
+    else:
+        return 0
 
 def main():
 
@@ -34,21 +24,37 @@ def main():
     for i in range(1, t + 1):
         ans = []
         S =[]
-        left = []
-        op=None
-        right = []
-        while True:
-            c = input()
+        for line in sys.stdin:
+            c = line.strip()
             if c == "":
+                while len(S) > 0:
+                    ans.append(S.pop())
                 print("".join(ans))
+                print()
                 break
-            if c == "(":
-                continue:
-            elif c is in ["1","2", "3", "4", "5","6","7", "8","9","0"]:
-                if op ==None:
-                    left.append(c)
-                else:
-                    right.append(c)
+            if c in ["1","2", "3", "4", "5","6","7", "8","9","0"]:
+                ans.append(c)
+            elif c == '(':
+                S.append(c)
+            elif c == ')':
+                while len(S) > 0:
+                    b = S.pop()
+                    if b =='(':
+                        break
+                    ans.append(b)
+            else:
+                while len(S) > 0 and prec(S[-1]) >= prec(c):
+                    ans.append(S.pop())
+                S.append(c)
+        
+        while len(S) > 0:
+            ans.append(S.pop())
+            print("".join(ans))
+            exit(0)
+
+
+                
+
         
         
 
