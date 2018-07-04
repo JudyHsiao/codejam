fc64967b57e6ebb0a6f9107c4ddf6d8e7690c376
JudyHsiao <cornsoup1984@gmail.com>
2018-07-04 08:01:13 +0800

    add case

--- uva/727.py
+++ uva/727.py
@@ -17,40 +17,49 @@ def prec(c):
     else:
         return 0
 
+def infixToPostfix(A):
+    ans = []
+    S = []
+    for c in A:
+        if c in ["1","2", "3", "4", "5","6","7", "8","9","0"]:
+                ans.append(c)
+        elif c == '(':
+            S.append(c)
+        elif c == ')':
+            while len(S) > 0:
+                b = S.pop()
+                if b =='(':
+                    break
+                ans.append(b)
+        else:
+            while len(S) > 0 and prec(S[-1]) >= prec(c):
+                ans.append(S.pop())
+            S.append(c)
+    
+    while len(S) > 0:
+        ans.append(S.pop())
+    return "".join(ans)
+
+
+
 def main():
 
     t = int(input())  # read a line with a single integer
     input()
-    for i in range(1, t + 1):
-        ans = []
-        S =[]
+    for i in range(t):
+        A=[]
         for line in sys.stdin:
             c = line.strip()
-            if c == "":
-                while len(S) > 0:
-                    ans.append(S.pop())
-                print("".join(ans))
-                print()
+            if c =="":
                 break
-            if c in ["1","2", "3", "4", "5","6","7", "8","9","0"]:
-                ans.append(c)
-            elif c == '(':
-                S.append(c)
-            elif c == ')':
-                while len(S) > 0:
-                    b = S.pop()
-                    if b =='(':
-                        break
-                    ans.append(b)
-            else:
-                while len(S) > 0 and prec(S[-1]) >= prec(c):
-                    ans.append(S.pop())
-                S.append(c)
-        
-        while len(S) > 0:
-            ans.append(S.pop())
-            print("".join(ans))
-            exit(0)
+            A.append(c)
+
+        print(infixToPostfix(A))
+        if i < t -1:
+            print()
+
+            
+    exit(0)
 
 
                 
