8a8bf3de9a7e6fea9720491f91167ef2c46cbb8e
Shu-Fang Hsiao <shu-fanghsiao@Shu-Fangde-MacBook-Pro-727.local>
2018-07-05 17:35:22 +0800

    add case

--- uva/10901.py
+++ uva/10901.py
@@ -9,6 +9,12 @@ if len(sys.argv) > 1:
     inp = ''.join(open(filename, "r").readlines())
     sys.stdin = io.StringIO(inp)
 
+def other_side(side):
+    if side == 0:
+        side =1
+    else:
+        side = 0
+    return side
 
 def main():
 
@@ -18,27 +24,52 @@ def main():
         L = collections.deque()
         R = collections.deque()
         for j in range(m):
-            [n, side] = [for s in input().split()]
+            [a, side] = [s for s in input().split()]
             if side == 'left':
-                L.append(int(n))
+                L.append((int(a),j))
             else:
-                R.append(int(n))
+                R.append((int(a),j))
 
         side = 0
         current_time = 0
         F = [L, R]
+        ans=[0] * m
         while len(F[0]) or len(F[1]):
             count = 0
-            while F[side][0] <=current_time and count < n:
-                print(current_time +t)
-                F[side].popleft()
+            while len(F[side]) and F[side][0][0] <=current_time and count < n:
+                car = F[side].popleft()
+                ans[car[1]] = current_time + t
                 count +=1
             
-            current_time +=t
-            if side == 0:
-                side =1
+            if count > 0:
+                side = other_side(side)
+                current_time +=t
             else:
-                side = 0
+                if len(L) > 0 and len(R) > 0:
+                    if F[other_side(side)][0][0] < F[side][0][0]:  
+                        current_time = max(F[other_side(side)][0][0], current_time) +t
+                        side = other_side(side)
+                    else:
+                        current_time = F[side][0][0]
+                elif len(L) == 0:
+                    current_time = max(R[0][0], current_time)
+                    if side != 1:
+                        current_time +=t
+                        side = other_side(side)
+                else:
+                    current_time = max(L[0][0], current_time)
+                    if side != 0:
+                        current_time +=t
+                        side = other_side(side)
+                   
+
+
+        
+        for j in range(m):
+            print(ans[j])
+
+        if i < T:
+            print()
 
 
 
