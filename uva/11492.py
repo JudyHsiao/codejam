c21ab075bed8f41bccc63e3c889ed95576278dd8
Shu-Fang Hsiao <shu-fanghsiao@Shu-Fangde-MacBook-Pro-727.local>
2018-07-09 18:06:26 +0800

    add case

--- uva/11492.py
+++ uva/11492.py
@@ -11,55 +11,64 @@ if len(sys.argv) > 1:
     inp = ''.join(open(filename, "r").readlines())
     sys.stdin = io.StringIO(inp)
 
-def dijkstra(G, V, s, t):
-    dist = defaultdict(dict)
-    for v in V:
-        dist(v) = float(inf)
-    dist[s] = 0
+class Graph:
+    def __init__(self):
+        self.V = set()
+        self.adj = collections.defaultdict(list)
 
-    spt = set()
-    heap = []
-    for v in V :
-        if v != s:
-            heapq.heappush(heap,[float("inf"),v])
-        else:
-            heapq.heappush(heap,[0,v])
+    def addEdge(self, src, dest, word):
+        newNode = [dest, word]
+        self.adj[src].append(newNode)
+ 
+        # Since graph is undirected, add an edge 
+        # from dest to src also
+        newNode = [src, word]
+        self.adj[dest].append(newNode)
 
-    for _ in range(len(V)):
-        (d, v) = heapq.heappop(heap)
-        dist[v] = d
-        spt.add(v)
-        for u, e in G[v].items():
-            if u not in spt() and dist[]
+def dijkstra(G, s):
+    dist = {}
+    for v in G.V:
+        dist[v] = [float("inf"), ""]
+    dist[s] = [0,""]
 
+    parent = {}
 
+    Q = []
+    heapq.heappush(Q, (dist[s][0], s, ""))
+    while len(Q):
+        print(Q)
+        d, u, word = heapq.heappop(Q)
+        if d == dist[u] :  
+            for v, w in G.adj[u]:
+                if dist[u] + len(w) < dist[v]:
+                    dist[v] = dist[u] + len(w)
+                    parent[v] = u
+                    heapq.heappush(Q, (dist[v], v, w))
 
-
+    return dist, parent
 
 
 
 def main():
     while True:
-
-        M= int(input)
-        if M == n :
+        M = int(input())
+        if M == 0 :
             break
 
         [O,D] = input().split()
-        G= collections.defaultdict(dict)
-        V= set()
+        G = Graph()
         for i in range(M):
-            [u, v, p] = input().split()
-            V.add(u)
-            V.add(v)
-            if v not in G[u] or len(G[u][v]) > len(p):
-                G[u][v] = p
-                G[v][u] = p
-        cost = dijkstra(G, V, O, D)
-        if cost > 0:
-            print(cost)
+            [u, v, w] = input().split()
+            G.addEdge(u,v,w)
+            G.V.add(u)
+            G.V.add(v)
+
+        dist, parent = dijkstra(G, O)
+        if dist[D] < float("inf"):
+            print(dist[D])    
         else:
-            print("impossivel")
+            print("impossivel") 
+            
 
 if __name__ == "__main__":
     main()
