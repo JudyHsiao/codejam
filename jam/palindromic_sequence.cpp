da22aaa4c3dc6c11835916abbaa7340ba0f09d2d
Judy <judy.hsiao@house-edge.com>
2018-10-05 07:36:52 +0800

    add

---
+++ jam/palindromic_sequence.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int T;
+    int kase = 0;
+    cin >> T ;
+
+    while (T--) {
+        int L, N ,K;
+        cin >> L >> N >> K;
+
+        
+    }
+    return 0;
+}
