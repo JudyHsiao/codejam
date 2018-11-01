fc03928619cb63ea7bd5c9c1d250660afd780712
Shu-Fang Hsiao <shu-fanghsiao@Shu-Fangde-MacBook-Pro-1024.local>
2018-11-01 10:28:23 +0800

    add

--- mylib/segment_tree_range_update.cpp
+++ mylib/segment_tree_range_update.cpp
@@ -1,4 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <assert.h>
+
 using namespace std;
 typedef long long ll;
 struct Node
@@ -55,4 +60,46 @@ ll kthlargest(ll k, int curr = 1, ll cstart = 0, ll cend = 1e9+10)
 	push(curr, mid-cstart+1, cend-mid);
 	if (rangetree[rangetree[curr].right].val >= k) return kthlargest(k, rangetree[curr].right, mid+1, cend);
 	else return kthlargest(k - rangetree[rangetree[curr].right].val, rangetree[curr].left, cstart, mid);
+}
+
+int main() {
+
+	//interval: [3,4] [2,8] [4,8] [1,3] [7,9] 左右閉區間
+	//  [9, 8, 8, 8, 7, 7, 7, 6, 6, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 1]
+	upto = 0;
+	newnode();
+	update(3,4);
+	update(2,8);
+	update(4,8);
+	update(1,3);
+	update(7,9);
+
+	int i5 = kthlargest(5);
+
+	int i1 = kthlargest(1);
+
+	int i30 = kthlargest(30);
+
+	cout << "5th: " << i5 << " 1st:" << i1 << " 30th:" << i30 << endl;
+
+	upto = 0;   //重新開始
+	newnode();  	// 重新開始
+	update(3,4);
+	update(2,8);
+	update(4,8);
+	update(1,3);
+	update(7,9);
+
+	i5 = kthlargest(5);
+
+	i1 = kthlargest(1);
+
+	i30 = kthlargest(30);
+
+	cout << "5th: " << i5 << " 1st:" << i1 << " 30th:" << i30 << endl;
+
+
+
+
+
 }
