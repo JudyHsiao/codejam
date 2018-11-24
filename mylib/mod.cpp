9985358c4914afb7a4a6e9b33d5b6d14b2abb01f
Judy <judy.hsiao@house-edge.com>
2018-11-24 20:28:54 +0800

    add

--- mylib/mod.cpp
+++ mylib/mod.cpp
@@ -1,6 +1,6 @@
 typedef unsigned long long ULL;
 
-
+const ULL MOD = 1000000007;
 
 void init_combination_mod() {
   const int maxn = 1000;
@@ -14,18 +14,39 @@ void init_combination_mod() {
   }
 }
 
-int pow_mod(ULL a, ULL b, int n) {
-  if(!b) return 1;
-  int k = pow_mod(a, b/2, n);
-  k = k * k % n;
-  if(b % 2) k = k * a % n;
-  return k;
+ULL pow_mod(ULL x, unsigned int y) 
+{ 
+    ULL res = 1;      // Initialize result 
+  
+    x = x % MOD;  // Update x if it is more than or  
+                // equal to p 
+  
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % MOD; 
+  
+        y = y>>1; // y = y/2 
+        x = (x*x) % MOD;   
+    } 
+    return res; 
+} 
+
+ULL factorial_mod(ULL n){
+    ULL res = 1;
+    for(ULL i = 1;i<=n;i++){
+        res *= i%MOD;
+        res = res % MOD;
+    }
+    return res;
 }
+
 
 int main() {
     ULL a = 18446744073709551615;
     ULL b =18446744073709551615;
     int n = 1000;
 
-    int p = pow_mod(a % n, b, n);   // p = a^b %n
+    int p = pow_mod(a % n, b);   // p = a^b %n
 }
