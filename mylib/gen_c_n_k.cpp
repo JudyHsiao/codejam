f95eaa0f0c2f36b5e181bd2c01e0c7f5f114234f
Judy <judy.hsiao@house-edge.com>
2018-12-11 16:45:04 +0800

    add

--- mylib/gen_c_n_k.cpp
+++ mylib/gen_c_n_k.cpp
@@ -2,6 +2,19 @@
 #include <algorithm>
 #include <vector>
 
+
+const int maxn = 100;
+long long c[maxn];
+long long Combination(int n, int k){
+    c[0] = 1;
+    for (int i =1; i <=n; i++) {
+        c[i] = c[i-1]*(n-i+1)/i;
+    }
+    return c[k];
+
+}
+
+
 int main() {
     int n, r;
     std::cin >> n;
--- mylib/prime.cpp
+++ mylib/prime.cpp
@@ -11,6 +11,18 @@ struct factor {
     factor(int a, int n):a(a),n(n){}
 };
 
+const int maxn = 10000000 + 10;
+
+int vis[maxn];
+void gen_primes(int n) {
+  int m = (int)sqrt(n+0.5);
+  int c = 0;
+  memset(vis, 0, sizeof(vis));
+  for(int i = 2; i <= m; i++) if(!vis[i]) {
+    for(int j = i*i; j <= n; j+=i) vis[j] = 1;
+  }
+}
+
 void prime_factor_decomposition(int n, vector<factor> &factors) {
     int m = floor(sqrt(n) + 0.5);
     for (int i = 2; i <= m; i++) {
@@ -26,9 +38,6 @@ void prime_factor_decomposition(int n, vector<factor> &factors) {
         if (n ==1) {
             break;
         }
-    }
-    if (n > 1) {
-         factors.push_back(factor(n, 1));
     }
 
 }
